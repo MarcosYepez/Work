fc6048e465d4cd48dae7d04dab37873540ed1e71
MarcosYepez <65257365+MarcosYepez@users.noreply.github.com>
2020-08-24 01:11:26 -0400

    PIPE.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Programa_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
